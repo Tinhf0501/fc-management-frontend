d69ef2ee1fde9ef72c714c28954dce1e0218f791
suongnv <suongnguyenytm2001@gmail.com>
2024-01-17 22:41:09 +0700

    build not-found-error page

--- src/app/router.ts
+++ src/app/router.ts
@@ -8,5 +8,9 @@ export const routes: Routes = [
     {
         path: '',
         loadChildren: () => import('./layout/main/man.module').then(m => m.MainModule)
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./pages/error/404/not-found.page').then(p => p.NotFoundPage)
     }
 ]
