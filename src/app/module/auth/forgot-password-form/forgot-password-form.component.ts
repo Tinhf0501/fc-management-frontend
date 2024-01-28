a9b5f2e02726bf480f5655d17dd89a920f610584
suongnv <suongnguyenytm2001@gmail.com>
2024-01-28 15:24:56 +0700

    i18n forget password page

--- src/app/module/auth/forgot-password-form/forgot-password-form.component.ts
+++ src/app/module/auth/forgot-password-form/forgot-password-form.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
     selector: 'forgot-password-form',
     templateUrl: './forgot-password-form.component.html',
     styleUrls: ['./forgot-password-form.component.scss'],
     standalone: true,
-    imports: [FormsModule, ReactiveFormsModule],
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        TranslateModule
+    ],
 })
 export class ForgotPasswordFormComponent implements OnInit {
     private formBuilder: FormBuilder = inject(FormBuilder);
